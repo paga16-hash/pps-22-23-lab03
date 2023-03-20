94211f88a903908b8fdc32b6d2e870b337a47f21
Alberto Paganelli <albi1600@gmail.com>
2023-03-20 11:55:38 +0100

    finished drop es1 tdd

---
+++ src/test/scala/lab03/ListTest.scala
@@ -0,0 +1,28 @@
+package lab03
+
+import org.junit.*
+import org.junit.Assert.*
+import lab03.Lists.*
+
+class ListTest:
+
+  import List.*
+
+  val l: List[Int] = Cons(10, Cons(20, Cons(30, Nil())))
+
+  @Test def testSum(): Unit =
+    assertEquals(0, sum(Nil()))
+    assertEquals(60, sum(l))
+
+  @Test def testMap(): Unit =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), map(l)(_ + 1))
+    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), map(l)(_ + ""))
+
+  @Test def testFilter(): Unit =
+    assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_ >= 20))
+    assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_ != 20))
+
+  @Test def testDrop(): Unit =
+    assertEquals(Cons(20, Cons(30, Nil())), drop(l, 1))
+    assertEquals(Cons(30, Nil()), drop(l, 2))
+    assertEquals(Nil(), drop(l, 5))
